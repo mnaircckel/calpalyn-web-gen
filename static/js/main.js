1e1096459c9813628eb57f16894008f78541d0d4
Marcel Champagne <champagn@grinnell.edu>
2017-07-14 15:22:45 -0500

    Expand middle container to 8

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@ Vue.component('box-container', {
     <div class="hero-body">
       <div class="container">
         <div class="columns is-vcentered">
-          <div class="column is-4 is-offset-4">
+          <div class="column is-8 is-offset-2">
             <div class="box message vform">
             <slot></slot>
             </div>
