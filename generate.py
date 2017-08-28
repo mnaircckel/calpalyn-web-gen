238aa55e57c72ee1d7c3a566d448aa26979a1ac0
Marcel Champagne <champagn@grinnell.edu>
2017-08-28 12:38:52 -0700

    Generation conversion

--- generate.py
+++ generate.py
@@ -1,14 +1,13 @@
 # Writes all data into Calpalyn instruction file
 # Data: Dictionary representing all form fields
-# Lines: A list of lines to write
 def write_lines(data):
     # Open file
     with open('file.instrs', 'wb+') as f:
         # Each line represents a line in the Calpalyn instruction file
 
         ''' Line 1 '''
-        dataA = '0' # File Type
-        dataB = data['line1Box2'] # Vertical Factor
+        dataA = '0'
+        dataB = data['line1Box2']
         f.write(dataA + ' '*(1-len(dataA)) +  ' ' + '0' + ' ' + dataB)
         f.write(" "*81+"//Line 01")
         f.write('\n')
@@ -20,21 +19,43 @@ def write_lines(data):
 
         ''' Line 3 '''
         dataA = '0'
-        dataB = data.next().get()[:1]
-        dataC = data.next().get()[:1]
-        dataD = data.next().get()[:1]
-        dataE = data.next().get()[:1]
-        dataF = data.next().get()[:1]
-        dataG = data.next().get()[:1]
-        dataH = data.next().get()[:1]
-        dataI = data.next().get()[:1]
+
+        dataB = '0'
+        if len(data['line3Box2']) > 0:
+            dataB = '1'
+
+        dataC = '0'
+        if len(data['line3Box3']) > 0:
+            dataC = '1'
+
+        dataD = '0'
+        if len(data['line3Box4']) > 0:
+            dataD = '1'
+
+        dataE = '0'
+        if len(data['line3Box5']) > 0:
+            dataE = '1'
+
+        dataF = '0'
+        if len(data['line3Box6']) > 0:
+            dataF = '1'
+
+        dataG = '0'
+        if len(data['line3Box7']) > 0:
+            dataG = '1'
+
+        dataH = '0'
+        if len(data['line3Box8']) > 0:
+            dataH = '1'
+
+        dataI = '0'
         f.write(dataA + ' '*(1-len(dataA)) + ' ' + dataB + ' '*(1-len(dataB))  + ' ' + dataC + ' '*(1-len(dataC))  + ' ' + dataD + ' '*(1-len(dataD))  + ' ' + dataE + ' '*(1-len(dataE))  + ' ' + dataF + ' '*(1-len(dataF))  + ' ' + dataG + ' '*(1-len(dataG))  + ' ' + dataH + ' '*(1-len(dataH))  + ' ' + dataI)
         f.write(" "*81+"//Line 03")
         f.write('\n')
 
-        ''' Subtotal Lines '''
+        ''' Subtotal Lines 5-11 '''
         for line in ["2", "3", "4", "5", "6", "7", "8"]:
-            f.write('1' + ' ' + data['line3Box' + line])
+            f.write('1' + ' ' + MANUALLYWRITELINE)
             f.write(" "*81+"//Lines 5-11")
             f.write('\n')
 
@@ -72,26 +93,27 @@ def write_lines(data):
             f.write('\n')
 
         ''' Lines 20, 20A '''
-        for i in range(entries_number/12):
+        for i in range(len(data['plots'])):
             # Line 20 entries
-            dataB = data.next().get()[:5]
-            dataC = data.next().get()[:1]
-            dataD = data.next().get()[:25]
-            dataF = data.next().get()[:1]
-            dataI = data.next().get()[:6]
-            dataJ = data.next().get()[:30]
+            dataB = data['pairs'][data['plots'][i]['line20Box1']]
+            dataC = data['plots'][i]['line20Box2']
+            dataD = data['plots'][i]['line20Box3']
+            dataF = data['plots'][i]['line20Box4']
+            dataI = data['plots'][i]['line20Box5']
+            dataJ = data['plots'][i]['line20Box6']
 
             # Line 20A entries
-            data1A = data.next().get()[:1]
-            data1B = data.next().get()[:1]
-            data1C = data.next().get()[:1]
-            data1D = data.next().get()[:7]
-            data1E = data.next().get()[:2]
-            data1F = data.next().get()[:1]
+            data1A = '0'
+            if i == 0 or data['plots'][i-1]['line20ABox2'] != data['plots'][i]['line20ABox2'] or data['plots'][i]['line20ABox2'] == '0':
+                data1A = '1'
+            data1B = data['plots'][i]['line20ABox2']
+            data1C = data['plots'][i]['line20ABox3']
+            data1D = data['plots'][i]['line20ABox4']
+            data1E = data['plots'][i]['line20ABox5']
+            data1F = data['plots'][i]['line20ABox6']
 
             # Line 20
-            if (i+1) != (entries_number/12):
-
+            if (i+1) != (len(data['plots'])):
                 f.write('0' + ' ' + dataB + ' '*(5-len(dataB)) + ' ' + dataC + ' '*(1-len(dataC))  + ' ' + dataD + ' '*(25-len(dataD)) + ' ' + dataF + ' '*(1-len(dataF))  + '      ' + dataI + ' '*(6-len(dataI)) + ' ' + dataJ )
             else:
                 f.write('1' + ' ' + dataB + ' '*(5-len(dataB)) + ' ' + dataC + ' '*(1-len(dataC))  + ' ' + dataD + ' '*(25-len(dataD)) + ' ' + dataF + ' '*(1-len(dataF))  + '      ' + dataI + ' '*(6-len(dataI)) + ' ' + dataJ )
@@ -140,13 +162,13 @@ def write_lines(data):
         f.write('\n')
 
         ''' Lines 26 '''
-        for i in range(entries_number/5):
-            dataB = data.next().get()[:7]
-            dataC = data.next().get()[:7]
-            dataD = data.next().get()[:7]
-            dataE = data.next().get()[:7]
-            dataF = data.next().get()[:8]
-            if (i+1) != (entries_number/5):
+        for i in range(len(data['dates'])):
+            dataB = data['dates'][i]['line26Box1']
+            dataC = data['dates'][i]['line26Box2']
+            dataD = data['dates'][i]['line26Box3']
+            dataE = data['dates'][i]['line26Box4']
+            dataF = data['dates'][i]['line26Box5']
+            if (i+1) != (len(data['dates'])):
                 f.write('0' + ' ' + dataB + ' '*(7-len(dataB)) + ' ' + dataC + ' '*(7-len(dataC)) + ' ' + dataD + ' '*(7-len(dataD)) + ' ' + dataE + ' '*(7-len(dataE)) + ' ' + dataF)
             else:
                 f.write('1' + ' ' + dataB + ' '*(7-len(dataB)) + ' ' + dataC + ' '*(7-len(dataC)) + ' ' + dataD + ' '*(7-len(dataD)) + ' ' + dataE + ' '*(7-len(dataE)) + ' ' + dataF)
@@ -159,13 +181,13 @@ def write_lines(data):
         f.write('\n')
 
         ''' Lines 27 '''
-        for i in range(entries_number/4):
-            dataB = data.next().get()[:7]
-            dataC = data.next().get()[:1]
-            dataD = data.next().get()[:7]
-            dataE = data.next().get()
+        for i in range(len(data['zones'])):
+            dataB = data['zones'][i]['line27Box1']
+            dataC = data['zones'][i]['line27Box2']
+            dataD = data['zones'][i]['line27Box3']
+            dataE = data['zones'][i]['line27Box4']
             num_dataE = int(math.ceil(len(dataE)/12.0))
-            if (i+1) != (entries_number/4):
+            if (i+1) != (len(data['zones'])):
                 f.write('0' + ' ' + dataB + ' '*(7-len(dataB)) + ' ' + dataC + ' '*(1-len(dataC))  + ' ' + dataD + ' '*(7-len(dataD)) + ' '*17)
             else:
                 f.write('1' + ' ' + dataB + ' '*(7-len(dataB)) + ' ' + dataC + ' '*(1-len(dataC))  + ' ' + dataD + ' '*(7-len(dataD)) + ' '*17)
@@ -185,10 +207,10 @@ def write_lines(data):
         f.write('\n')
 
         ''' Lines 30 '''
-        for i in range(entries_number/2):
-            dataB = data.next().get()[:7]
-            dataC = data.next().get()[:1]
-            if (i+1) != (entries_number/2):
+        for i in range(len(data['lines'])):
+            dataB = data['lines'][i]['line30Box1']
+            dataC = data['lines'][i]['line30Box2']
+            if (i+1) != (len(data['lines'])):
                 f.write('0' + ' ' + dataB + ' '*(7-len(dataB)) + ' ' + dataC)
             else:
                 f.write('1' + ' ' + dataB + ' '*(7-len(dataB)) + ' ' + dataC)
